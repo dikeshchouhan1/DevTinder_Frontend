980fe5fd8e5f5dd5517e03fcf0fa802b5ec70db5
dikeshchouhan <103301921+dikeshchouhan1@users.noreply.github.com>
2025-10-22 00:14:22 +0530

    Update BASE_URL to a hardcoded value

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 // src/utils/constants.js
 
-export const BASE_URL = import.meta.env.VITE_BASE_URL ;
+// export const BASE_URL = import.meta.env.VITE_BASE_URL ;
+export const BASE_URL = "https://devtinder-backend-jt6d.onrender.com";
+
 
