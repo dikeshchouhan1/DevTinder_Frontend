b3808b4bb6baafefc03795b2108be5afed82d43b
dikeshchouhan <103301921+dikeshchouhan1@users.noreply.github.com>
2025-10-22 00:22:12 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,3 @@
 // src/utils/constants.js
 
-// export const BASE_URL = import.meta.env.VITE_BASE_URL ;
-export const BASE_URL = "https://devtinder-backend-jt6d.onrender.com";
-
-
+export const BASE_URL = import.meta.env.VITE_BASE_URL ;
